6fc0e7bcba25f9ceff6b25d2d4fd9d36b5a4e9ae
Kamilla <kamilla.kumysheva@mail.ru>
2023-02-07 15:55:59 +0300

    Добавьте файлы проекта.

---
+++ Pages/customer/PersonalPage2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BakeryKotlov.Pages.customer
+{
+    /// <summary>
+    /// Логика взаимодействия для PersonalPage2.xaml
+    /// </summary>
+    public partial class PersonalPage2 : Page
+    {
+        public PersonalPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
